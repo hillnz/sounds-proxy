35b3d9b45c189f6bc23e4e4eea192f5f97d7bfe0
Jono Hill <jonathan@hillnz.com>
2022-05-25 22:05:10 +0000

    wip

--- test/s3.sh
+++ test/s3.sh
@@ -16,6 +16,14 @@ finally() {
 
 trap finally EXIT
 
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_REGION=ap-southeast-2
+export SOUNDS_PROXY_BASE_URL=http://localhost:3000
+export SOUNDS_PROXY_S3_BUCKET=sounds-proxy-test
+export SOUNDS_PROXY_S3_ENDPOINT_URL=http://localhost:4566
+export RUST_LOG=sounds_proxy=debug
+
 docker_id=$(docker run -d -it --rm \
     -p 4566:4566 \
     -e LOCALSTACK_SERVICES=s3 \
@@ -25,17 +33,10 @@ while ! aws --endpoint-url=http://localhost:4566 s3 ls; do
     sleep 1
 done
 
-export AWS_ACCESS_KEY=test
-export AWS_SECRET_KEY=test
-export AWS_REGION=ap-southeast-2
-export SOUNDS_PROXY_BASE_URL=http://localhost:3000
-export SOUNDS_PROXY_S3_BUCKET=sounds-proxy-test
-export SOUNDS_PROXY_S3_ENDPOINT_URL=http://localhost:4566
-export RUST_LOG=sounds_proxy=debug
 
 aws --endpoint-url=http://localhost:4566 s3 mb "s3://$SOUNDS_PROXY_S3_BUCKET" || true
 
-cargo run &
+cargo run & 
 proxy_pid=$!
 
 sleep 5
